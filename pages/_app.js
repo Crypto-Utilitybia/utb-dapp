ea2633fdbef74f14b294646c259f9d640cd8faf4
levan-k <levanimarkkikilashvili@gmail.com>
2021-07-11 09:21:20 -0700

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
+import Head from 'next/head'
+import { ApolloProvider } from 'react-apollo'
+import { client } from '@lib/queries'
+import Header from '@components/Header/Header'
+import Footer from '@components/Footer/Footer'
 import '@styles/globals.css'
+import '@styles/common.css'
 
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Head>
+        <title>Crypto Utilitybia</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header title="Welcome to my app!" />
+
+      <main>
+        <Component {...pageProps} />
+      </main>
+
+      <Footer />
+    </ApolloProvider>
+  )
 }
 
 export default Application
